d426151e09afe0ab3db3c29bdc0033d2fcae6c6a
Zhuxian <meizhuxi@amazon.com>
2020-06-06 10:06:12 -0400

    comment out event output

--- handler.js
+++ handler.js
@@ -13,7 +13,7 @@ module.exports.hello = async event => {
     body: JSON.stringify(
       {
         message: "Go Serverless v1.0! Your function executed successfully!",
-        input: event
+       // input: event
       },
       null,
       2
